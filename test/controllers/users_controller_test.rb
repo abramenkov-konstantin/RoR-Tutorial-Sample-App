b250e512ce5acefdf02482031955e368f4a51143
Konstantin_Abramenkov <constantine@123software.ru>
2016-08-09 17:26:25 +0300

    v2

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -24,4 +24,14 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to login_url
   end
 
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+        user: { password:              "12345678",
+                password_confirmation: "12345678",
+                admin: true } }
+    assert_not @other_user.michael.admin?
+  end
+
 end
